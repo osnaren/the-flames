c68867d458719c6af87ef0b294d16434cd858ae3
osnaren <66naren@gmail.com>
2025-04-27 12:22:21 +0530

    refactor: remove deprecated components and update file structure for clarity

--- src/components/FloatingControlPanel.tsx
+++
@@ -1,8 +0,0 @@
-/**
- * @deprecated This component has been moved to 'src/components/layout/FloatingControlPanel'
- * This file remains only for backward compatibility and should not be used in new code.
- * It will be removed in a future release.
- */
-
-import FloatingControlPanel from './layout/FloatingControlPanel';
-export default FloatingControlPanel;
--- src/components/GlobalCharts.tsx
+++
@@ -1,8 +0,0 @@
-/**
- * @deprecated This component has been moved to 'src/components/layout/GlobalCharts'
- * This file remains only for backward compatibility and should not be used in new code.
- * It will be removed in a future release.
- */
-
-import GlobalCharts from './layout/GlobalCharts';
-export default GlobalCharts;
