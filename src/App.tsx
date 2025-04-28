8520b5a9bc537a6432475975e0019509f8430900
osnaren <66naren@gmail.com>
2025-04-28 20:43:47 +0530

    refactor: Remove animation state management from App component for cleaner code

--- src/App.tsx
+++ src/App.tsx
@@ -6,22 +6,17 @@ import HomePage from '@pages/HomePage';
 import HowItWorksPage from '@pages/HowItWorksPage';
 import ManualModePage from '@pages/ManualModePage';
 import FlameBackground from '@ui/FlameBackground';
-import { useState } from 'react';
 import { Route, Routes } from 'react-router-dom';
 
 function App() {
-  const [animationsEnabled, setAnimationsEnabled] = useState(true);
-
   return (
     <div className="from-customBg-1 to-customBg-2 relative flex min-h-screen flex-col bg-gradient-to-br transition-colors duration-500">
-      <FlameBackground animationsEnabled={animationsEnabled} />
+      <FlameBackground />
       <Navbar />
-
-      <FloatingControlPanel animationsEnabled={animationsEnabled} setAnimationsEnabled={setAnimationsEnabled} />
 
       <main className="pt-6">
         <Routes>
-          <Route path="/" element={<HomePage animationsEnabled={animationsEnabled} />} />
+          <Route path="/" element={<HomePage />} />
           <Route path="/charts" element={<ChartsPage />} />
           <Route path="/how-it-works" element={<HowItWorksPage />} />
           <Route path="/manual" element={<ManualModePage />} />
